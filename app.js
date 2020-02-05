1ec632e0d203117837ead0ce3bcbdb33ee343d44
Abinash Dutta <abinashdtt45@gmail.com>
2020-02-05 15:49:06 +0530

    intro video added

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-//Hi This is Abinash. We are going to learn basic git skills to work on projects.
-//First we do git init and add the parent repository- not always necessary 
-// 1. Learn to fork and clone.
-// 2. Learn to add remote head.
-// 3. Learn to fetch and rebase.
-// 4. Learn to manage conflicts.
-// 5. Learn to add, commit and push.
-// 6. Learn to generate pull request and merge with master.
+const express = require('express')
+const bodyParser = require('body-parser')
+var app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-// Inititial Assignment
-// 1. Arvinder Singh
-// 2. Debojit Das
-// 3. Ritu Agarwal
-// 4. Vartika Varshney
-// 5. Abinash Dutta
+//Start of Get Requests
+
+app.get('/',(req, res)=>{
+	res.render('login')
+})
 
 
-//Sort of Deadline- 26th February and then before 15th March
+//End of get requests
 
-//This is Me.
+
+
+
+//Start of post requrests
+
+
+
+
+
+//End of post requests
+
+
+
+app.listen(3000, ()=>{
+	console.log("Connected to Server")
+})
