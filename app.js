a94baf281bff01c5eb549d144ba88adbd8d9aef0
Abinash Dutta <abinashdtt45@gmail.com>
2020-03-07 11:21:04 +0530

    just a try

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+if( typeof localStorage === "undefined" || localStorage === null){
+	var LocalStorage = require('node-localstorage').LocalStorage
+	localStorage = new LocalStorage('./scratch')
+}
+
 
 //Start of Get Requests
 
 app.get('/',(req, res)=>{
+	var name = localStorage.getItem('name')
+	var pass = localStorage.getItem('pass')
+	console.log(name)
+	localStorage.clear()
+	console.log(localStorage.getItem('name'))
 	res.render('home')
 })
 app.get('/about',(req, res)=>{
@@ -29,7 +39,18 @@ app.get('/signin',(req, res)=>{
 
 
 //End of get requests
-
+app.post("/abcd", (req,res)=>{
+	console.log(req.body)
+	var name = req.body.email
+	var pass = req.body.password
+	localStorage.setItem("name" , name)
+	localStorage.setItem("pass" , pass)
+	// console.log(name+" "+pass)
+	res.redirect("/")
+	console.log(req.body)
+	
+	
+})
 
 
 
